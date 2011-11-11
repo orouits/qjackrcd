c3dd38e3d7e943f975fadf3fa9532bcb71b6447a
orouits <orouits@f9062a65-8197-4bcc-993a-5e3b93611f54>
2011-11-11 13:49:34 +0000

    headers

--- recorder.h
+++ recorder.h
@@ -1,3 +1,4 @@
+// $Id$
 /***************************************************************************
  *   Copyright (C) 2011 - Olivier ROUITS                                   *
  *                                                                         *
@@ -16,6 +17,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+// $LastChangedBy$
+// $LastChangedRevision$
+// $LastChangeDate$
+// $HeadURL$
 
 #ifndef RECORDER_H
 #define RECORDER_H
