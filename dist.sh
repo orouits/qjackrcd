f361ebdfe4e29e138cb0e7954abc87e3bd637f4b
orouits <orouits@f9062a65-8197-4bcc-993a-5e3b93611f54>
2011-11-23 23:05:14 +0000

    dist

--- dist.sh
+++ dist.sh
@@ -2,13 +2,18 @@
 
 VERSION=$1
 
-[ -d .tmp ] || exit
 [ -n ${VERSION} ] || exit
 
+make clean
 make
+mkdir .tmp
 rm -rf .tmp/qjackrcd-${VERSION}
 mkdir -p .tmp/qjackrcd-${VERSION}
 cp qjackrcd .tmp/qjackrcd-${VERSION}
 cd .tmp
 tar -czvf qjackrcd-${VERSION}.tar.gz qjackrcd-${VERSION}
+mv qjackrcd-${VERSION}.tar.gz ..
+cd ..
+rm -rf .tmp/qjackrcd-${VERSION}
+
 
