866bf1c818d8e7a0ec1189452b3c33ae2c20c636
orouits <orouits@f9062a65-8197-4bcc-993a-5e3b93611f54>
2011-12-04 21:56:45 +0000

    dist includes descktop file

--- dist.sh
+++ dist.sh
@@ -23,7 +23,7 @@ cd ..
 rm -rf .tmp
 mkdir -p .tmp/qjackrcd-${VERSION}
 
-cp *.ui *.h *.pro *.pro.user *.cpp *.ts *.png *.qrc *.sh Makefile README .tmp/qjackrcd-${VERSION}
+cp *.ui *.h *.pro *.pro.user *.cpp *.ts *.png *.qrc *.sh *.desktop Makefile README .tmp/qjackrcd-${VERSION}
 cd .tmp
 tar -czvf qjackrcd-src-${VERSION}.tar.gz qjackrcd-${VERSION}
 mv qjackrcd-src-${VERSION}.tar.gz ..
