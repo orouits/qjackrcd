b656fd54c85b6569b5b205d01f0d8240db629fec
orouits <orouits@f9062a65-8197-4bcc-993a-5e3b93611f54>
2011-12-29 12:43:13 +0000

    add CS translation

---
+++ qjackrcd_cs.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>%p%</source>
+        <translation>%p%</translation>
+    </message>
+    <message>
+        <source>Disk</source>
+        <translation>Disk</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Ukončit</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Soubor</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>Připraven</translation>
+    </message>
+    <message>
+        <source>QJackRcd</source>
+        <translation>QJackRcd</translation>
+    </message>
+    <message>
+        <source>Split file mode</source>
+        <translation>Režim rozdělení souboru</translation>
+    </message>
+    <message>
+        <source>Auto pause</source>
+        <translation>Pozastavit automaticky</translation>
+    </message>
+    <message>
+        <source>Post processing</source>
+        <translation>Další zpracování</translation>
+    </message>
+    <message>
+        <source>Action</source>
+        <translation>Krok</translation>
+    </message>
+    <message>
+        <source>Activation delay (s)</source>
+        <translation>Zpoždění spuštění (s)</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Vlastní</translation>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation>Nahrávat</translation>
+    </message>
+    <message>
+        <source>Waiting for sound...</source>
+        <translation>Čeká se na zvuk...</translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation>Příkaz</translation>
+    </message>
+    <message>
+        <source>Last file</source>
+        <translation>Poslední soubor</translation>
+    </message>
+    <message>
+        <source>Threshold (dB)</source>
+        <translation>Práh (dB)</translation>
+    </message>
+    <message>
+        <source>Recording...</source>
+        <translation>Nahrává se...</translation>
+    </message>
+</context>
+</TS>
