4fbee89ad069b9566a102d419eb0d8df098038dc
orouits <orouits@f9062a65-8197-4bcc-993a-5e3b93611f54>
2011-12-30 17:58:51 +0000

    qjackrcd-SNAPSHOT

--- dist.sh
+++ dist.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 RELEASE="${1:-SNAPSHOT}"
-PUBLISH="${2:-np}"
 
 ARCNAME="qjackrcd-${RELEASE}"
 
 echo "### Clean all"
-rm "qjackrcd-*.tar.gz" &>/dev/null
+rm qjackrcd-*.tar.gz &>/dev/null
 rm -rf ".tmp" &>/dev/null
 make distclean  &>/dev/null
 
@@ -24,26 +23,24 @@ cd ..
 
 rm -rf ".tmp"
 
-if [[ "${PUBLISH}" == "p" ]]
+if [[ "${RELEASE}" == "SNAPSHOT" ]]
 then
-    if [[ "${RELEASE}" == "SNAPSHOT" ]]
-    then
-        echo "### Commit in SVN"
-        svn ci -m "${ARCNAME}"
-        echo "### publish ${ARCNAME} archive to TESTING"
-        scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/testing/"
-    elif [[ "${RELEASE}" =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]]
-    then
-        echo "### Make tag in SVN /tags/${ARCNAME}"
-        svn cp . "svn+ssh://orouits@svn.code.sf.net/p/qjackrcd/code/tags/${ARCNAME}"
-        echo "### publish ${ARCNAME} archive to STABLE"
-        scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/stable/"
-    elif [[ "${RELEASE}" =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+[[:alpha:]]$ ]]
-    then
-        echo "### Make tag in SVN /tags/${ARCNAME}"
-        svn cp . "svn+ssh://orouits@svn.code.sf.net/p/qjackrcd/code/tags/${ARCNAME}"
-        echo "### publish ${ARCNAME} archive to TESTING"
-        scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/testing/"
-    fi
+    echo "### Commit in SVN"
+    svn ci -m "${ARCNAME}"
+    echo "### publish ${ARCNAME} archive to TESTING"
+    scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/testing/"
+elif [[ "${RELEASE}" =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]]
+then
+    echo "### Make tag in SVN /tags/${ARCNAME}"
+    svn cp . "svn+ssh://orouits@svn.code.sf.net/p/qjackrcd/code/tags/${ARCNAME}"
+    echo "### publish ${ARCNAME} archive to STABLE"
+    scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/stable/"
+elif [[ "${RELEASE}" =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+[[:alpha:]]$ ]]
+then
+    echo "### Make tag in SVN /tags/${ARCNAME}"
+    svn cp . "svn+ssh://orouits@svn.code.sf.net/p/qjackrcd/code/tags/${ARCNAME}"
+    echo "### publish ${ARCNAME} archive to TESTING"
+    scp "${ARCNAME}.tar.gz" "orouits,qjackrcd@frs.sourceforge.net:/home/frs/project/q/qj/qjackrcd/testing/"
 fi
+
 echo "### Done."
