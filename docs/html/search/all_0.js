4c534c78887555ce4516cb5cad3f8cfc462c1a3c
Olivier <olivier.rouits@free.fr>
2017-12-30 20:49:06 +0100

    doxygen pass

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -3,6 +3,8 @@ var searchData=
   ['main',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
   ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]],
   ['mainconsole',['MainConsole',['../classMainConsole.html',1,'']]],
+  ['mainconsole_2ecpp',['mainconsole.cpp',['../mainconsole_8cpp.html',1,'']]],
+  ['mainconsole_2eh',['mainconsole.h',['../mainconsole_8h.html',1,'']]],
   ['mainwindow',['MainWindow',['../classMainWindow.html',1,'']]],
   ['mainwindow_2ecpp',['mainwindow.cpp',['../mainwindow_8cpp.html',1,'']]],
   ['mainwindow_2eh',['mainwindow.h',['../mainwindow_8h.html',1,'']]]
